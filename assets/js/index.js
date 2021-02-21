5d7c0b6d9d816a0d994cb661fa1e75da9e8892e7
Arthur Lui <luiarthur@gmail.com>
2021-02-20 19:54:03 -0800

    added a few slies

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+import 'https://code.jquery.com/jquery-3.5.1.min.js'
+import { handleDirection, Slider } from './pager.js'
+import './citefrombib.min.js'
+import "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+import "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js"
+
+// Initialize the slider. If an id is provided, then use that as starting page.
+const initPageIdGetter = window.location.href.split('#')
+const firstPage = initPageIdGetter.length > 1 ? initPageIdGetter[1]*1 : 1
+const slider = new Slider(firstPage)
+
+// This handles slide navigation.
+$(document).on("keydown", function (e) {
+  handleDirection(e, slider)
+});
+
+// citefrombib
+citefrombib.make()
+
+// KaTeX
+renderMathInElement(document.body)
+$(document).ready(function () { // this need jquery
+  renderMathInElement(document.body, {
+    // ...options...
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ]
+  });
+});
---
+++ assets/js/pager.js
@@ -0,0 +1,73 @@
+// A slider for slide transitions.
+export class Slider {
+  constructor(currentPage=1) {
+    this.currentPage = currentPage
+    this.extraPages = $('div.backup-slides section').length
+    this.totalPages = $('section').length - this.extraPages
+    this.realTotalPages = this.totalPages + this.extraPages
+    this.previousPage = 0
+    makePager()
+    this.setVisibility()
+    this.verbose = false 
+    this.setSectionId()
+  }
+
+  setSectionId() {
+    for (let i = 0; i < this.realTotalPages; i++) {
+      $("section").eq(i).attr('id', i)
+    }
+  }
+
+  increment() {
+    if (this.currentPage < this.realTotalPages) {
+      this.previousPage = this.currentPage
+      this.currentPage += 1
+      this.setVisibility()
+    }
+  }
+
+  decrement() {
+    if (this.currentPage > 1) {
+      this.previousPage = this.currentPage
+      this.currentPage -= 1
+      this.setVisibility()
+    }
+  }
+
+  setVisibility() {
+    this.previousPage > 0 && $("section").eq(this.previousPage - 1).css("display", "none")
+    $("section").eq(this.currentPage - 1).css("display", "block")
+
+    $("#current-page").text(this.currentPage)
+    if (this.currentPage > this.totalPages) {
+      $("#total-pages").text(this.realTotalPages)
+    } else {
+      $("#total-pages").text(this.totalPages)
+    }
+
+    this.verbose && console.log("setting visibility")
+  }
+}
+
+export function handleDirection(e, slider) {
+  const leftKeys = [8 ,37, 72]  // backspace, left, h
+  const rightKeys = [13, 32, 39, 76]  // enter, space, right, l
+
+  if (leftKeys.includes(e.which)) {
+    slider.decrement()
+  } else if (rightKeys.includes(e.which)) {
+    slider.increment()
+  }
+
+  // Set query string
+  window.location.href = window.location.href.split('#')[0] + "#" + slider.currentPage
+}
+
+// Page counter.
+export function makePager() {
+  const pager = `
+  <div id="page-counter">
+    <span id="current-page"></span> / <span id="total-pages"></span>
+  </div>`
+  $('.slide-deck').before(pager)
+}
